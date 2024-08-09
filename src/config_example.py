12a65c5c129fef2b36b4eae707801d78a8cde2ee
saneness <saneness@protonmail.com>
2024-08-09 17:19:28 +0100

    updates

---
+++ src/config_example.py
@@ -0,0 +1,10 @@
+#!!! remove `_example` from the name before start !!!
+###     Telegram-client side:   ###
+api_id    = api_id
+api_hash  = "api_hash"
+MyChannel = "mychannel" #link to your chat 
+
+###          Bot-side:          ###
+bot_token     = "bot_token"
+admin_chat_id = None       #Secure option
+channel_id    = None
